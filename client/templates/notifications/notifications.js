6bad3b064a252f8b0673751aa4cb8ddc91e412a1
Chris Lu <gogreen.chris@gmail.com>
2015-07-13 23:54:23 -0400

    fixed notifications bug. Added infinite pagination feature.

--- client/templates/notifications/notifications.js
+++ client/templates/notifications/notifications.js
@@ -11,10 +11,11 @@ Template.notificationItem.helpers({
     notificationPostPath: function() {
         return Router.routes.postPage.path({_id: this.postId});
     }
-});
+})
 
 Template.notificationItem.events({
     'click a': function() {
+        console.log('trying to update');
         Notifications.update(this._id, {$set: {read: true}});
     }
-});
+})
